c64dcf7b6e0b98b7322d9157e0c6c5af30bb9c55
Dasha Sarnatskaya <dasha.sarnatskaya@gmail.com>
2021-12-20 02:01:52 +0300

    update

--- lab10/src/test/java/HomePageTests.java
+++ lab10/src/test/java/HomePageTests.java
@@ -4,12 +4,15 @@ import org.testng.annotations.Test;
 import page.BelaviaHomePage;
 import page.BelaviaHomeResultPage;
 
+import page.TimetableResultPage;
 import service.LocationsCreator;
 
-public class BelaviaSearchTicketsTest extends CommonConditions {
+public class HomePageTests extends CommonConditions {
 
     private final String EXPECTED_AIRPORT = "SVO";
     private final String EXPECTED_PRICE = "132,73 BYN";
+    private final String EXPECTED_FLIGHT_NUMBER = "B2 942";
+    private final String EXPECTED_ARRIVAL_TIME = "09:10";
 
     @Test
     public void searchTicketsOnWayTripTest() {
@@ -43,6 +46,36 @@ public class BelaviaSearchTicketsTest extends CommonConditions {
                 .clickOnSearchButton();
 
         Assert.assertEquals(resultPage.getPrice(), EXPECTED_PRICE);
+
+    }
+
+    @Test
+    public void viewSuggestions() throws InterruptedException {
+        BelaviaHomePage homePage = new BelaviaHomePage(driver);
+        String flightNumber = homePage.openPage()
+                .clickOnSuggestionsButton()
+                .clickOnLocation()
+                .getFlightNumber();
+        Assert.assertEquals(flightNumber,EXPECTED_FLIGHT_NUMBER);
+    }
+
+    @Test
+    public void viewTimetable() throws InterruptedException {
+        BelaviaHomePage belaviaHomePage = new BelaviaHomePage(driver);
+        Location testLocations = LocationsCreator.locationsFromProperty();
+        TimetableResultPage timetablePage = new TimetableResultPage(driver);
+        belaviaHomePage.openPage()
+                .clickOnMenuButton()
+                .clickOnTimeTableButton()
+                .clickOnOneWayButton()
+                .clickOnDeparturePlace()
+                .inputDeparturePlace(testLocations.getDeparturePlace())
+                .clickOnDestinationPlace()
+                .inputDestination(testLocations.getDestinationPlace())
+                .clickOnCalendarButton()
+                .clickOnDepartDateButton()
+                .clickOnSearchButton();
+        Assert.assertEquals( timetablePage.getArrivalTime(),EXPECTED_ARRIVAL_TIME);
 
     }
 }
