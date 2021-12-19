d4f2caeedee7941e15669ef4edb36804ce47f22e
Dasha Sarnatskaya <dasha.sarnatskaya@gmail.com>
2021-12-19 04:34:44 +0300

    update

--- lab10/src/test/java/BelaviaViewTest.java
+++ lab10/src/test/java/BelaviaViewTest.java
@@ -18,22 +18,22 @@ public class BelaviaViewTest extends CommonConditions {
         Assert.assertEquals(EXPECTED_FLIGHT_NUMBER, flightNumber);
     }
 
-    @Test
-    public void viewTimetable() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        BelaviaHomePage belaviaHomePage = new BelaviaHomePage(driver);
-        TimetablePage timetablePage = new TimetablePage(driver);
-        belaviaHomePage.openPage();
-        Thread.sleep(2000);
-        //belaviaHomePage.clickOnMenuButton();
-        Thread.sleep(2000);
-        //belaviaHomePage.clickOnTimetableButton();
-        Thread.sleep(2000);
-        timetablePage.clickOnOneWayButton();
-        Thread.sleep(2000);
-        timetablePage.clickOnTudaCalendarButton();
-        Thread.sleep(2000);
-        timetablePage.clickOnObratnoCalendarButton();
-        // new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@id='select-main-menu']"))).click();
-    }
+//    @Test
+//    public void viewTimetable() throws InterruptedException {
+//        WebDriver driver = new ChromeDriver();
+//        BelaviaHomePage belaviaHomePage = new BelaviaHomePage(driver);
+//        TimetablePage timetablePage = new TimetablePage(driver);
+//        belaviaHomePage.openPage();
+//        Thread.sleep(2000);
+//        //belaviaHomePage.clickOnMenuButton();
+//        Thread.sleep(2000);
+//        //belaviaHomePage.clickOnTimetableButton();
+//        Thread.sleep(2000);
+//        timetablePage.clickOnOneWayButton();
+//        Thread.sleep(2000);
+//        timetablePage.clickOnTudaCalendarButton();
+//        Thread.sleep(2000);
+//        timetablePage.clickOnObratnoCalendarButton();
+//        // new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@id='select-main-menu']"))).click();
+//    }
 }
