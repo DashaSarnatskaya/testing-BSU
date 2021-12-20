328c074e67fd2d4e66c017e0597d6c7235714f0b
Dasha Sarnatskaya <dasha.sarnatskaya@gmail.com>
2021-12-20 04:31:37 +0300

    update

---
+++ lab10/src/test/java/SearchTicketsTest.java
@@ -0,0 +1,47 @@
+import model.Location;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import page.BelaviaHomePage;
+import page.BelaviaHomeResultPage;
+import service.LocationsCreator;
+
+public class SearchTicketsTest  extends CommonConditions{
+    private final String EXPECTED_AIRPORT = "SVO";
+    private final String EXPECTED_PRICE = "132,73 BYN";
+    @Test
+    public void searchTicketsOnWayTripTest() {
+        Location testLocations = LocationsCreator.locationsFromProperty();
+        BelaviaHomePage homePage = new BelaviaHomePage(driver);
+        BelaviaHomeResultPage resultPage = homePage.openPage()
+                .clickOnDeparturePlace()
+                .inputDeparturePlace(testLocations.getDeparturePlace())
+                .clickOnDestinationPlace()
+                .inputDestination(testLocations.getDestinationPlace())
+                .clickOnOneWayButton()
+                .clickOnCalendarButton()
+                .clickOnDepartDateButton()
+                .clickOnSearchButton();
+
+        Assert.assertEquals(resultPage.getAirportName(), EXPECTED_AIRPORT);
+    }
+
+    @Test
+    public void searchTicketsRoundTripTest() {
+        Location testLocations = LocationsCreator.locationsFromProperty();
+        BelaviaHomePage homePage = new BelaviaHomePage(driver);
+        BelaviaHomeResultPage resultPage = homePage.openPage()
+                .clickOnDeparturePlace()
+                .inputDeparturePlace(testLocations.getDeparturePlace())
+                .clickOnDestinationPlace()
+                .inputDestination(testLocations.getDestinationPlace())
+                .clickOnRoundTripButton()
+                .clickOnDepartDateButton()
+                .clickOnReturnDateButton()
+                .clickOnSearchButton();
+
+        Assert.assertEquals(resultPage.getPrice(), EXPECTED_PRICE);
+
+    }
+
+
+}
